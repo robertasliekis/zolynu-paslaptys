08cd95d38a7e57adba581ee6735dd9b8471905a8
Robertas Liekis <robertasliekis@gmail.com>
2020-10-02 17:08:44 +0300

    Updates

---
+++ precache-manifest.83d22bd6ae8f7cdef643c36b45c4a1ce.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3def5969739a4c67490cebe8fc97c006",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/index.html"
+  },
+  {
+    "revision": "08c64c080e139c778673",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/css/main.f7fa8f3b.chunk.css"
+  },
+  {
+    "revision": "18bb60b4fdbdb37e462a",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/js/2.aab99417.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/js/2.aab99417.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08c64c080e139c778673",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/js/main.d1bacc4f.chunk.js"
+  },
+  {
+    "revision": "375ca10e70c5940d34da",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/js/runtime-main.23083dbb.js"
+  },
+  {
+    "revision": "3964e99f55c6566f5f2a72d60d1a7e9b",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/Interaktyvi_knyga_išklotinė.3964e99f.mp4"
+  },
+  {
+    "revision": "8e402d0335060d56d6ca051f16ddd885",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/ajeras1.8e402d03.jpg"
+  },
+  {
+    "revision": "b734cd643c86acf57cab42a9a1777de7",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/ajeras2.b734cd64.jpg"
+  },
+  {
+    "revision": "6eaf19d6cde5f6b4c6a778ef470404fc",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/ajeras3.6eaf19d6.jpg"
+  },
+  {
+    "revision": "9b2d31ebbd5b3442016627ec9d5050c8",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/dilgele1.9b2d31eb.jpg"
+  },
+  {
+    "revision": "20ff2a20caca9d4df3b2d5e9309e9570",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/dilgele2.20ff2a20.jpg"
+  },
+  {
+    "revision": "eed07b2acc65ab33682b01d225e976f3",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/dilgele3.eed07b2a.jpg"
+  },
+  {
+    "revision": "40a3f29689c1761af54c2c9b12c45e15",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/dobilas1.40a3f296.jpg"
+  },
+  {
+    "revision": "3576f5bee9e19a2b1ea5b5378aa962fd",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/dobilas2.3576f5be.jpg"
+  },
+  {
+    "revision": "d889b1a57c8e8fb1316f469c10cbb735",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/dobilas3.d889b1a5.jpg"
+  },
+  {
+    "revision": "f24fa4680a8550c2c954aaedccb04150",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/pakalnute1.f24fa468.jpg"
+  },
+  {
+    "revision": "26b531bfb6d7478f85fb097231064316",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/pakalnute2.26b531bf.jpg"
+  },
+  {
+    "revision": "0af14ce50ef1a60208898666d094ee8b",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/pakalnute3.0af14ce5.jpg"
+  },
+  {
+    "revision": "f8a06eb12a14455ff10d0bdedb3fcfb9",
+    "url": "//robertasliekis.github.io/zolynu-paslaptys/static/media/video_screenshot.f8a06eb1.png"
+  }
+]);
